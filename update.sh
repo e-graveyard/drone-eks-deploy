a6f3f70f93405a47630bde612dbb38e4a18a346a
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-10-26 15:43:53 -0300

    fix: don't use backticks, don't export credentials as it conflicts w/ "aws-iam-authenticator"

--- update.sh
+++ update.sh
@@ -15,20 +15,14 @@ if [ -z ${PLUGIN_MANIFEST} ]; then
     exit 1
 fi
 
-
 if [ -z ${PLUGIN_AWS_REGION} ]; then
     # Try to pull the region from the host that is running Drone - this assumes
     # the Drone EC2 instance is in the same region as the EKS cluster you are
     # deploying onto. If needed, override with PLUGIN_AWS_REGION param,
-    export AWS_REGION_AND_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
-    export PLUGIN_AWS_REGION=`echo ${AWS_REGION_AND_ZONE} | sed 's/[a-z]$//'`
+    export AWS_REGION_AND_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+    export PLUGIN_AWS_REGION=$(echo ${AWS_REGION_AND_ZONE} | sed 's/[a-z]$//')
 fi
 export AWS_DEFAULT_REGION=${PLUGIN_AWS_REGION}
-
-
-echo "Exporting credentials..."
-export AWS_ACCESS_KEY_ID=${PLUGIN_ACCESS_KEY}
-export AWS_SECRET_ACCESS_KEY=${PLUGIN_SECRET_KEY}
 
 
 echo "Fetching the authentication token..."
