10aec21355a9e8a78b1223b374ce21f4682c63a8
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-10-31 19:54:13 -0300

    feat: export empty AWS credentials

--- update.sh
+++ update.sh
@@ -78,6 +78,10 @@ echo "Exporting k8s configuration path..."
 export KUBECONFIG=$KUBECONFIG:~/.kube/config
 
 
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+
+
 echo "Applying the manifest..."
 echo ""
 cat ${PLUGIN_MANIFEST} | kubectl apply -f -
